71142f0d4d172130656243ea1e0eab7bb6f50ae3
iskandaryansona <iskandaryansona1999@gmail.com>
2024-02-01 17:52:10 +0400

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Sona on 01.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
